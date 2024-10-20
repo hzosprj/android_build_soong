53bebc6b9376fb3a0e9a466059810fd1bd05c6a0
Michael Bestas <mkbestas@lineageos.org>
2024-10-20 07:20:42 +0300

    gen_build_prop: Set ro.hertzify.device
    Change-Id: I485d74642b869d7ac94ef2fd171cafd759965d41

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -118,6 +118,8 @@ def parse_args():
   if args.build_thumbprint_file:
     config["BuildThumbprint"] = args.build_thumbprint_file.read().strip()
 
+  config["HertzifyDevice"] = config["DeviceName"]
+
   override_config(config)
 
   append_additional_system_props(args)
@@ -244,6 +246,8 @@ def generate_build_info(args):
   # Only add _asan for a sanitized build if it isn't already a part of the
   # flavor (via a dedicated lunch config for example).
   print(f"ro.build.flavor={config['BuildFlavor']}")
+
+  print(f"ro.hertzify.device={config['HertzifyDevice']}")
 
   # These values are deprecated, use "ro.product.cpu.abilist"
   # instead (see below).

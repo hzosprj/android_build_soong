6458190c10a59844dd087122c070d31ad114eca1
HZOS PRJ <hzosprj@gmail.com>
2025-09-24 01:06:16 +0000

    m

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -223,7 +223,7 @@ def generate_build_info(args):
       print(f"ro.build.display.id?={config['BuildId']} {config['BuildKeys']}")
   else:
     # Non-user builds should show detailed build information (See build desc above)
-    print(f"ro.build.display.id?={config['PixelageDesc']}")
+    print(f"ro.build.display.id?={config['HertzifyDesc']}")
   print(f"ro.build.version.incremental={config['BuildNumber']}")
   print(f"ro.build.version.sdk={config['Platform_sdk_version']}")
   print(f"ro.build.version.sdk_full={config['Platform_sdk_version_full']}")
